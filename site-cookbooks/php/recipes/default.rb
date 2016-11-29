27a340fd911acc49d1e35d751eb4c6e4c4dbaa2b
Kazunori ITO <k.ito@kuniei-act.com>
2016-11-29 10:27:59 +0900

    php5.6に変更

--- site-cookbooks/php/recipes/default.rb
+++ site-cookbooks/php/recipes/default.rb
@@ -21,10 +21,10 @@
 
 #%w{php php-devel php-pear php-mbstring php-xml php-mcrypt php-gd php-pecl-xdebug php-opcache php-pecl-apcu php-phpunit-PHPUnit php-mysqlnd}.each do |pkg|
 
-%w{php php-devel php-pear php-mbstring php-xml php-mcrypt php-gd php-pecl-xdebug php-opcache php-pecl-apcu php-mysqlnd php-intl}.each do |pkg|
+%w{php php-devel php-pear php-mbstring php-xml php-mcrypt php-gd php-opcache php-pecl-apcu php-mysqlnd php-intl}.each do |pkg|
   package pkg do
     action [ :install, :upgrade ]
-    options "--enablerepo=remi,remi-php55"
+    options "--enablerepo=remi,remi-php56"
   end
 end
 
