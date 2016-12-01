b5e6989330f0fb5ba03aa38eb7a7bb0a1594c876
Kazunori ITO <k.ito@kuniei-act.com>
2016-12-01 15:43:15 +0900

    apacheの設定追加

--- site-cookbooks/apache/recipes/default.rb
+++ site-cookbooks/apache/recipes/default.rb
@@ -11,9 +11,28 @@ package "httpd" do
   action [ :install, :upgrade ]
 end
 
+
+bash "backup httpd.conf" do
+  code <<-EOC
+    cp -p /etc/httpd/conf/httpd.conf /tmp/httpd.conf.`date "+%Y%m%d_%H%M%S"`
+  EOC
+end
+
+template "httpd.conf" do
+  path "/etc/httpd/conf/httpd.conf"
+  source "httpd.conf.erb"
+end
+
+bash "backup janus.conf" do
+  code <<-EOC
+    cp -p /etc/httpd/conf.d/janus.conf /tmp/janus.conf.`date "+%Y%m%d_%H%M%S"`
+  EOC
+end
+
 template "janus.conf" do
   path "/etc/httpd/conf.d/janus.conf"
   source "janus.conf.erb"
+  #not_if { File.exists?("/etc/httpd/conf.d/janus.conf") }
 end
 
 service "httpd" do
