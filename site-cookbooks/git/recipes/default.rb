b88dede5f2adf6c78014cd75a66bf1f17e594f3f
Kazunori ITO <k.ito@kuniei-act.com>
2017-01-20 14:41:33 +0900

    slいれてみた

--- site-cookbooks/git/recipes/default.rb
+++ site-cookbooks/git/recipes/default.rb
@@ -10,3 +10,19 @@
 package "git" do
   action [ :install, :upgrade ]
 end
+
+package "ncurses-devel" do
+  action [ :install, :upgrade ]
+end
+
+bash 'install_sl' do
+  not_if { File.exists?("/usr/local/bin/sl") }
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+    git clone https://github.com/gmkou/sl.git
+    cd sl
+    make
+    mv /tmp/sl/sl /usr/local/bin/sl
+  EOH
+end
